59c2ad64e7dce72496b783bff40c8777d784efdc
ShaundDev <shaun@s-desmond.tech>
2021-11-24 04:03:12 +0100

    Initial Cleanup. CountryStateView & 50 Construction Slots.

---
+++ common/defines/uao_defines.lua
@@ -0,0 +1,24 @@
+--- Mod Start/End Date
+NDefines.NGame.START_DATE = "1936.1.1.12"
+NDefines.NGame.END_DATE = "1947.1.1.1"
+
+--- Division Designer Change
+--NDefines.NMilitary.MAX_DIVISION_BRIGADE_WIDTH = 5; -- Max width of regiments in division designer.
+--NDefines.NMilitary.MAX_DIVISION_BRIGADE_HEIGHT = 4; -- Max height of regiments in division designer.
+--NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 2; -- Max width of support in division designer.
+--NDefines.NMilitary.MAX_DIVISION_SUPPORT_HEIGHT = 4; -- Max height of support in division designer.
+
+-- Production License Base Change
+NDefines.NDiplomacy.LICENSE_ACCEPTANCE_OPINION_FACTOR = 0.3;
+NDefines.NDiplomacy.LICENSE_ACCEPTANCE_TECH_DIFFERENCE_BASE = 19;
+
+-- Construction Slots
+NDefines.NBuildings.MAX_SHARED_SLOTS = 50;				-- Max slots shared by factories
+NDefines.NBuildings.MAX_BUILDING_LEVELS = 50;			-- Max levels a building can have.
+
+-- Production Efficiency Change
+NDefines.NProduction.BASE_FACTORY_START_EFFICIENCY_FACTOR = 10; -- Base start efficiency for factories expressed in %.
+NDefines.NProduction.BASE_FACTORY_MAX_EFFICIENCY_FACTOR = 30;	-- Base max efficiency for factories expressed in %.
+
+-- Technology Ahead of time research
+NDefines.NTechnology.BASE_YEAR_AHEAD_PENALTY_FACTOR = 3;
